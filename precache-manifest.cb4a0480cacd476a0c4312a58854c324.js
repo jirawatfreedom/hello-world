8850643bd957f14d8865df54d91b4f9f746f2242
Jirawat <jirawat@entercorp.net>
2019-01-14 14:58:46 +0700

    Updates

---
+++ precache-manifest.cb4a0480cacd476a0c4312a58854c324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4c156e95faa4f420480",
+    "url": "/hello-world/static/css/main.3fb19d90.chunk.css"
+  },
+  {
+    "revision": "b4c156e95faa4f420480",
+    "url": "/hello-world/static/js/main.b4c156e9.chunk.js"
+  },
+  {
+    "revision": "184dd399a405f491855c",
+    "url": "/hello-world/static/js/1.184dd399.chunk.js"
+  },
+  {
+    "revision": "ec2a5d419092358b57e1",
+    "url": "/hello-world/static/js/runtime~main.ec2a5d41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eb4299442381ad57c477b96edffebd7d",
+    "url": "/hello-world/index.html"
+  }
+];
